0059d00f082f6010dbb3d77e8221839adacb67c2
jbauthess <authesse@gmail.com>
2025-09-04 10:53:41 +0200

    fix modernized UNET model unittest

--- src/model/tests/test_unet.py
+++ src/model/tests/test_unet.py
@@ -8,10 +8,10 @@ def test_model_architecture() -> None:
     M = UNetModel(1, 5)
 
     # simulate an image
-    I = torch.ones((1, 1, 572, 572))
+    I = torch.ones((1, 1, 384, 384))
 
     # run inference
     x = M(I)
 
     # check output size is valid
-    assert x.size() == (1, 5, 388, 388)
+    assert x.size() == (1, 5, 384, 384)
