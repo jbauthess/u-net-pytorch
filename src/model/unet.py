0dbf2f3f5cfae6ad7bce350dcb4e464727b49352
jbauthess <authesse@gmail.com>
2025-09-05 12:19:04 +0200

    set the number of featuremaps of the model parameterizable.

--- src/model/unet.py
+++ src/model/unet.py
@@ -10,27 +10,27 @@ from .blocks import (
 
 
 class UNetModel(torch.nn.Module):
-    def __init__(self, nb_in_channels: int, nb_classes: int):
+    def __init__(self, nb_in_channels: int, nb_classes: int, base_fm_number: int = 64):
         super(UNetModel, self).__init__()
 
         # blocks corresponding to the contracting path
         self.compute_fm = create_convolutional_block(
-            nb_in_channels, CONV_KERNEL_SIZE, 64
+            nb_in_channels, CONV_KERNEL_SIZE, base_fm_number
         )
 
-        self.down1 = create_downsampling_block(64, 128)
-        self.down2 = create_downsampling_block(128, 256)
-        self.down3 = create_downsampling_block(256, 512)
-        self.down4 = create_downsampling_block(512, 1024)
+        self.down1 = create_downsampling_block(base_fm_number, base_fm_number * 2)
+        self.down2 = create_downsampling_block(base_fm_number * 2, base_fm_number * 4)
+        self.down3 = create_downsampling_block(base_fm_number * 4, base_fm_number * 8)
+        self.down4 = create_downsampling_block(base_fm_number * 8, base_fm_number * 16)
 
         # blocks corresponding to expansive path
-        self.up1 = create_upsampling_block(1024, 512)
-        self.up2 = create_upsampling_block(512, 256)
-        self.up3 = create_upsampling_block(256, 128)
-        self.up4 = create_upsampling_block(128, 64)
+        self.up1 = create_upsampling_block(base_fm_number * 16, base_fm_number * 8)
+        self.up2 = create_upsampling_block(base_fm_number * 8, base_fm_number * 4)
+        self.up3 = create_upsampling_block(base_fm_number * 4, base_fm_number * 2)
+        self.up4 = create_upsampling_block(base_fm_number * 2, base_fm_number)
 
         # prediction
-        self.out = Conv2d(64, nb_classes, (1, 1), 1, padding=0)
+        self.out = Conv2d(base_fm_number, nb_classes, (1, 1), 1, padding=0)
 
     def forward(self, x):
         # coompress the information (encoder)
